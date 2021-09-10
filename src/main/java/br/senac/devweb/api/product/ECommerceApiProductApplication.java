99c9fc3ad2927758c5893c907967edd6605c31fb
Rômulo Pereira <romulo.stads@hotmail.com>
2021-09-10 20:58:32 -0300

    Setup inicial da aplicação

---
+++ src/main/java/br/senac/devweb/api/product/ECommerceApiProductApplication.java
@@ -0,0 +1,13 @@
+package br.senac.devweb.api.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApiProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApiProductApplication.class, args);
+	}
+
+}
