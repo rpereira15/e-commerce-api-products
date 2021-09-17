a552eeb8dd96d2666774701f351a8002d9596dfc
Guilherme <guilherme-mattia123@hotmail.com>
2021-09-16 21:44:56 -0300

    criação da classe categoria e implementação do filtro

--- src/main/java/br/senac/devweb/api/product/categoria/CategoriaService.java
+++ src/main/java/br/senac/devweb/api/product/categoria/CategoriaService.java
@@ -1,9 +1,10 @@
 package br.senac.devweb.api.product.categoria;
 
+import com.querydsl.core.types.Predicate;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -19,5 +20,16 @@ public class CategoriaService {
                 .status(Categoria.Status.ATIVO)
                 .build());
 
+    }
+
+    public List<Categoria> getAllCategoria(Predicate filter) {
+      return this.categoriaRepository.findAll(filter);
+    }
+
+    public void deleteCategoria(Long id) {
+        Categoria categoria = this.categoriaRepository.findById(id).get();
+
+        categoria.setStatus(Categoria.Status.INATIVO);
+        this.categoriaRepository.save(categoria);
     }
 }
