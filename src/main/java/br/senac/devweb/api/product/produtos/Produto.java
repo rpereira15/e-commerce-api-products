fa902aa7a475f0820c59a7614fb3edab781357db
Rômulo Pereira <romulo.stads@hotmail.com>
2021-09-21 20:14:53 -0300

    Atualização aula 21-09-2021 (Produtos 1)

---
+++ src/main/java/br/senac/devweb/api/product/produtos/Produto.java
@@ -0,0 +1,85 @@
+package br.senac.devweb.api.product.produtos;
+
+
+import br.senac.devweb.api.product.categoria.Categoria;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "PRODUTO")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O campo nome não pode ser nulo")
+    @Size(min = 1, max = 30, message = "O campo nome deve conter entre 1 e 30 caracteres")
+    @Column(name = "NOME")
+    private String nome;
+
+    @NotNull(message = "O campo descricao não pode ser nulo")
+    @Size(min = 1, max = 255, message = "O campo descrição deve conter entre 1 e 255 caracteres")
+    @Column(name = "DESCRICAO")
+    private String descricao;
+
+    @Column(name = "COMPLEMENTO")
+    private String complemento;
+
+    @NotNull(message = "O campo valor não pode ser nulo")
+    @NotEmpty(message = "O campo valor de medida não pode ser vazio")
+    @Column(name = "VALOR")
+    private Double valor;
+
+    @NotNull(message = "O campo unidade de medida não pode ser nulo")
+    @NotEmpty(message = "O campo unidade de medida não pode ser vazio")
+    @Column(name = "UNIDADE_MEDIDA")
+    private UnidadeMedida unidadeMedida;
+
+    @NotNull(message = "O campo quantidade não pode ser nulo")
+    @NotEmpty(message = "O campo quantidade não pode ser vazio")
+    @Column(name = "QTDE")
+    private Double qtde;
+
+    @NotNull(message = "O campo fabricante não pode ser nulo")
+    @Size(min = 1, max = 255, message = "O campo fabricante deve conter entre 1 e 255 caracteres")
+    @Column(name = "FABRICANTE")
+    private String fabricante;
+
+    @Column(name = "FORNECEDOR")
+    private String fornecedor;
+
+    @NotNull(message = "O campo status não pode ser nulo")
+    @NotEmpty(message = "O campo status não pode ser vazio")
+    @Column(name = "STATUS")
+    private Status status;
+
+    @NotNull(message = "A categoria é obrigatória")
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Categoria.class)
+    @JoinColumn(name = "idCategoria" )
+    private Categoria categoria;
+
+    public enum UnidadeMedida {
+        UN,
+        KG,
+        ML,
+        TN,
+        MT,
+        MT2,
+        MT3,
+        LT
+    }
+
+    public enum Status{
+        INATIVO,
+        ATIVO
+    }
+}
