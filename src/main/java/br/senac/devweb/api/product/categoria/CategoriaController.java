badd7f10c69acdfd771065c16c3968fb98a669a1
Rômulo Pereira <romulo.stads@hotmail.com>
2021-11-25 20:20:29 -0300

    produto cadastrar

--- src/main/java/br/senac/devweb/api/product/categoria/CategoriaController.java
+++ src/main/java/br/senac/devweb/api/product/categoria/CategoriaController.java
@@ -1,25 +1,32 @@
 package br.senac.devweb.api.product.categoria;
 
+import br.senac.devweb.api.product.util.Paginacao;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import javassist.NotFoundException;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/categoria")
 @AllArgsConstructor
+@CrossOrigin("*")
 public class CategoriaController {
 
     private CategoriaService categoriaService;
+    private CategoriaRepository categoriaRepository;
 
     @PostMapping("/")
     public ResponseEntity<CategoriaRepresentation.Detail> createCategoria(
-           @Valid @RequestBody CategoriaRepresentation.CreateOrUpdateCategoria createOrUpdateCategoria) {
+            @Valid @RequestBody CategoriaRepresentation.CreateOrUpdateCategoria createOrUpdateCategoria) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(CategoriaRepresentation.Detail.from(this.categoriaService.salvar(createOrUpdateCategoria)));
@@ -27,19 +34,45 @@ public class CategoriaController {
 
     @PutMapping("/{id}")
     public ResponseEntity<CategoriaRepresentation.Detail> atualizaCategoria(@PathVariable("id") Long id,
-          @Valid @RequestBody CategoriaRepresentation.CreateOrUpdateCategoria createOrUpdateCategoria) {
+                                                                            @Valid @RequestBody CategoriaRepresentation.CreateOrUpdateCategoria createOrUpdateCategoria) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(CategoriaRepresentation.Detail.from(this.categoriaService.update(id, createOrUpdateCategoria)));
     }
 
     @GetMapping("/")
+    public ResponseEntity<Paginacao> getAll(
+            @RequestParam(name = "filtro", required = false, defaultValue = "") String filtro,
+            @RequestParam(name = "paginaSelecionada", defaultValue = "0") Integer paginaSelecionada,
+            @RequestParam(name = "tamanhoPagina", defaultValue = "20") Integer tamanhoPagina) {
+
+        BooleanExpression filter = Strings.isEmpty(filtro) ? QCategoria.categoria.status.eq(Categoria.Status.ATIVO) :
+                QCategoria.categoria.status.eq(Categoria.Status.ATIVO)
+                        .and(QCategoria.categoria.descricao.containsIgnoreCase(filtro));
+
+        Pageable pageRequest = PageRequest.of(paginaSelecionada, tamanhoPagina);
+
+        Page<Categoria> categoriaList = this.categoriaRepository.findAll(filter, pageRequest);
+
+        Paginacao paginacao = Paginacao.builder()
+                .conteudo(CategoriaRepresentation.Lista
+                        .from(categoriaList.getContent()))
+                .paginaSelecionada(paginaSelecionada)
+                .tamanhoPagina(tamanhoPagina)
+                .proximaPagina(categoriaList.hasNext())
+                .build();
+
+        return ResponseEntity.ok(paginacao);
+    }
+
+    @GetMapping("/full-categoria")
     public ResponseEntity<List<CategoriaRepresentation.Lista>> getAll() {
 
         BooleanExpression filter = QCategoria.categoria.status.eq(Categoria.Status.ATIVO);
 
-        return ResponseEntity.ok(CategoriaRepresentation.Lista
-                .from(this.categoriaService.getAllCategoria(filter)));
+        List<Categoria> all = this.categoriaRepository.findAll(filter);
+
+        return ResponseEntity.ok(CategoriaRepresentation.Lista.from(all));
     }
 
     @GetMapping("/{id}")
